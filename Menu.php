4533d049f77622fa00373d0308f983142b5789f7
pekka18 <wallydignos18@gmail.com>
2023-10-21 16:04:10 +0800

    [second commit]

--- Menu.php
+++ Menu.php
@@ -4,17 +4,21 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-      Bootstrap
-    </a>
+    <a class="navbar-brand">Jollibot</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
-<?php
-echo "My first PHP script!";
-?>
+<h2>Create Menu</h2>
+<h6>Menu Name</6>
+<input id="menuName" class="form-control form-control-sm" type="text" aria-label=".form-control-sm example">
+<h6>Menu Description</6>
+<textarea id="menuDesc" class="form-control"></textarea>
+<button type="button" class="btn btn-primary">Submit</button>
 
 </body>
 </html>
